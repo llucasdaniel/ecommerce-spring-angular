6ea1a3407fdfcbff9c33610dcaabe4e50519cb98
Lucas Daniel <lucasdaniel@Lucass-MacBook-Pro.local>
2021-03-08 17:52:48 +0000

    Login using OKTA dev

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc:{
+        clientId: '0oaartk33Ijjz6LZ45d6',
+        issuer: 'https://dev-00493115.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
